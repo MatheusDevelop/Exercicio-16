c912e038225b7cbbc420f1d40dfe86e56569c23f
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-13 13:41:06 -0300

    ...

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula16Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto sapato = new Produto();
+            sapato.Nome = "Versace";
+
+            Produto chinelo = new Produto(45);
+            System.Console.WriteLine(chinelo.Codigo);
+
+            Produto camisa = new Produto(34, "Nike", "Preto", 294);
+            System.Console.WriteLine(camisa.Codigo);
+            System.Console.WriteLine(camisa.Nome);
+            System.Console.WriteLine(camisa.Descricao);
+            System.Console.WriteLine(camisa.Estoque);
+
+        }
+    }
+}
